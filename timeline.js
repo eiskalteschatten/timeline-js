cc952f4c6bbd2b2347c807f2efc99040276ce281
Alex Seifert <alex@alexseifert.com>
2022-12-01 19:58:13 +0100

    Start the gradient

--- timeline.js
+++ timeline.js
@@ -6,11 +6,13 @@ function drawTimeline() {
   }
 
   const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const numberOfSpaces = 5;
+  const numberOfSpaces = 10;
 
   const ctx = canvas.getContext('2d');
+  const foregroundColor = isDarkMode ? 'white' : 'black';
+  const backgroundColor = isDarkMode ? 'black' : 'white';
 
-  ctx.strokeStyle = isDarkMode ? 'white' : 'black';
+  ctx.strokeStyle = foregroundColor;
 
   function drawLines() {
     const distanceBetweenLines = canvas.width / numberOfSpaces;
@@ -31,14 +33,29 @@ function drawTimeline() {
 
     // Draw horizontal line
     ctx.beginPath();
-    ctx.setTransform(1,0,0,1, 0, canvas.height / 2);
+    ctx.setTransform(1, 0, 0, 1, 0, canvas.height / 2);
     ctx.moveTo(0, 0);
     ctx.lineTo(canvas.width, 0);
     ctx.stroke();
     ctx.closePath();
   }
 
+  function drawGradient() {
+    const gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    gradient.addColorStop(0, backgroundColor);
+    gradient.addColorStop(.15, 'rgba(0,0,0,0)');
+    gradient.addColorStop(.85, 'rgba(0,0,0,0)');
+    gradient.addColorStop(1, backgroundColor);
+
+    ctx.beginPath();
+    ctx.fillStyle = gradient;
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.closePath();
+  }
+
   drawLines();
+  drawGradient();
 }
 
 window.onload = drawTimeline;
